3447ecb0866faae66401a761ea0ebb9c34791f46
WangYan-Good <987639823@qq.com>
2026-04-03 21:16:46 +0800

    revert: 恢复ROOT权限检查并标记为TODO
    - 恢复 install.sh 中的 ROOT 权限检查
    - 标记为 TODO 待后续处理

--- install.sh
+++ install.sh
@@ -31,9 +31,9 @@ warn() {
     echo -e "\n$is_warn $@\n"
 }
 
-## >>> start: 修复BUG #1:安全：强制 ROOT 权限
+## >>> start: TODO 修复BUG #1:安全：强制 ROOT 权限
 # root
-# [[ $EUID != 0 ]] && err "当前非 ${yellow}ROOT用户.${none}"
+[[ $EUID != 0 ]] && err "当前非 ${yellow}ROOT用户.${none}"
 ## <<< end
 
 # yum or apt-get, ubuntu/debian/centos
