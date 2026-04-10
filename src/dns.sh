84e4909be9a1527eb61447cfc47174bc627952c6
WangYan-Good <987639823@qq.com>
2026-04-11 00:22:27 +0800

    fix(T19): fix SC2206 and SC2046 in dns.sh
    - Quote array expansion: is_tmp_list=("${is_dns_list[@]}")
    - Quote command substitutions and variables (SC2046)

--- src/dns.sh
+++ src/dns.sh
@@ -42,16 +42,16 @@ dns_set() {
             ;;
         esac
     else
-        is_tmp_list=(${is_dns_list[@]})
+        is_tmp_list=("${is_dns_list[@]}")
         ask list is_dns_use null "\n请选择 DNS:\n"
         if [[ $is_dns_use == "set" ]]; then
             ask string is_dns_use "请输入 DNS: "
         fi
     fi
     if [[ $is_dns_use == "none" ]]; then
-        cat <<<$(jq '.dns={}' $is_config_json) >$is_config_json
+        cat <<<"$(jq '.dns={}' "$is_config_json")" >"$is_config_json"
     else
-        cat <<<$(jq '.dns.servers=["'${is_dns_use/https/https+local}'"]' $is_config_json) >$is_config_json
+        cat <<<"$(jq '.dns.servers=["'"${is_dns_use/https/https+local}"'"]' "$is_config_json")" >"$is_config_json"
     fi
     manage restart &
     msg "\n已更新 DNS 为: $(_green $is_dns_use)\n"
